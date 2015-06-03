3751f5c49b10549ee65b111baf5c7f87c84cd361
kinslayer <kinslayer.e@gmail.com>
2015-06-02 22:42:21 -0300

    mlplanets version no inicial no funcionando

--- src/main/java/mlplanets/Application.java
+++ src/main/java/mlplanets/Application.java
@@ -1,24 +1,11 @@
 package mlplanets;
 
-import mlplanets.dao.CelestialObjectDAO;
-import mlplanets.dao.SolarSystemDAO;
-import mlplanets.dao.impl.SolarSystemDAOImpl;
-import mlplanets.domain.CelestialObject;
-import mlplanets.domain.SolarSystem;
-import mlplanets.service.CelestialObjectService;
-import mlplanets.service.OrbitService;
-import mlplanets.util.CelestialObjectFactory;
-import mlplanets.util.OrbitFactory;
-import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Set;
 
 @Component
 public class Application {
